dce9f54a3ec5b4a318cd0e52f3d329b2b916ee4a
Jared Mullin <JaredMullin@protonmail.com>
2021-10-28 15:44:48 +0100

    Altered Driver class to work with new library Added helper methods to register students, modules and courses. Better formatted printing.

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,46 +3,65 @@ import com.github.lophixyz.Module;
 import com.github.lophixyz.Student;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
 public class Driver {
+
   public static void main(String[] args) {
     Course computerScience = new Course("Computer Science and Information Technology", DateTime.parse("2021-09-06"), DateTime.parse("2022-05-07"));
     Module softwareEngineering = new Module("CT417", "Software Engineering III");
     Module advancedProfessionalSkills = new Module("CT436", "Advanced Professional Skills");
     Module informationRetrieval = new Module("CT4100", "Information Retrieval");
     Module machineLearning = new Module("CT4101", "Machine Learning");
-    computerScience.addModule(softwareEngineering);
-    softwareEngineering.addCourse(computerScience);
-    computerScience.addModule(advancedProfessionalSkills);
-    advancedProfessionalSkills.addCourse(computerScience);
-    computerScience.addModule(informationRetrieval);
-    informationRetrieval.addCourse(computerScience);
-    computerScience.addModule(machineLearning);
-    machineLearning.addCourse(computerScience);
+    addModuleToCourse(softwareEngineering, computerScience);
+    addModuleToCourse(advancedProfessionalSkills, computerScience);
+    addModuleToCourse(informationRetrieval, computerScience);
+    addModuleToCourse(machineLearning, computerScience);
 
     Student johnSmith = new Student( "John", "Smith", 22, DateTime.parse("1999-01-01"));
-    computerScience.registerStudent(johnSmith);
-    softwareEngineering.registerStudent(johnSmith);
-    advancedProfessionalSkills.registerStudent(johnSmith);
-    informationRetrieval.registerStudent(johnSmith);
+    registerStudentForCourse(johnSmith, computerScience);
+    registerStudentForModule(johnSmith, softwareEngineering);
+    registerStudentForModule(johnSmith, advancedProfessionalSkills);
+    registerStudentForModule(johnSmith, informationRetrieval);
 
     Student marySue = new Student("Mary", "Sue", 21, DateTime.parse("2000-12-15"));
-    computerScience.registerStudent(marySue);
-    softwareEngineering.registerStudent(marySue);
-    advancedProfessionalSkills.registerStudent(marySue);
-    machineLearning.registerStudent(marySue);
+    registerStudentForCourse(marySue, computerScience);
+    registerStudentForModule(marySue, softwareEngineering);
+    registerStudentForModule(marySue, informationRetrieval);
+    registerStudentForModule(marySue, machineLearning);
 
     System.out.printf("Course: %s\n", computerScience.getName());
-    System.out.println("Modules:");
+    System.out.printf("%s's Modules: \n", computerScience.getName());
+    System.out.format("\t\t%10s %40s\n", "Module ID", "Module Name");
     for (Module module: computerScience.getModules()) {
-      System.out.printf("%s - %s\n", module.getId(), module.getName());
+      System.out.format("\t\t%10s %40s\n", module.getId(), module.getName());
     }
     System.out.println("Students:");
-    System.out.format("%8s %15s %12s %12s %15s\n", "ID", "Username", "First Name", "Last Name", "Date of Birth");
+    System.out.format("\t\t%3s %15s %50s\n", "ID", "Username", "Course");
     for (Student student: computerScience.getStudents()) {
-      System.out.format("%8s %15s %12s %12s %15s\n", student.getStudentID(), student.getUsername(), student.getFirstName(), student.getLastName(), student.getDob().toString(
-          DateTimeFormat.shortDate()));
+      System.out.format("\t\t%3s %15s %50s\n", student.getStudentID(), student.getUsername(), student.getCourse().getName());
+    }
+    for (Student student: computerScience.getStudents()) {
+      System.out.printf("%s's Registered Modules:\n", student.getUsername());
+      System.out.format("\t\t%10s %40s\n", "Module ID", "Module Name");
+      for (Module module : student.getModules()) {
+        System.out.format("\t\t%10s %40s\n", module.getId(), module.getName());
+      }
     }
   }
+
+  public static void registerStudentForModule(Student student, Module module) {
+    module.registerStudent(student);
+    student.addModule(module);
+  }
+
+  public static void registerStudentForCourse(Student student, Course course) {
+    course.registerStudent(student);
+    student.setCourse(course);
+  }
+
+  public static void addModuleToCourse(Module module, Course course) {
+    course.addModule(module);
+    module.addCourse(course);
+  }
+
 }
